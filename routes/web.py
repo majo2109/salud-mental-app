12de052416d0c94e052a61d3abb9c56e0016fbd1
majo2109 <majorincon2109@gmail.com>
2025-12-04 14:31:07 -0500

    web editar

--- routes/web.py
+++ routes/web.py
@@ -451,3 +451,17 @@ def recuperar_evaluacion_web(
     session.commit()
     return RedirectResponse(url="/evaluaciones/inactivas", status_code=303)
 
+@router.get("/deportistas/{deportista_id}/editar", response_class=HTMLResponse)
+def form_editar_deportista(
+    deportista_id: int,
+    request: Request,
+    session: Session = Depends(get_session),
+):
+    deportista = session.get(Deportista, deportista_id)
+    if not deportista:
+        raise HTTPException(status_code=404, detail="Deportista no encontrado")
+
+    return templates.TemplateResponse(
+        "editar_deportista.html",
+        {"request": request, "deportista": deportista},
+    )
